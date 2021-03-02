5260e9a4f3dbc9663c9d316bc80a20c4bffd6766
Ryu1845 <ryu@tpgjbo.xyz>
2021-03-02 15:45:46 +0100

    Changes to the static HTML code. Mostly making code prettier

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 const STREAM_CALENDAR_CONFIG = {
   "documentTitle": "Stream Calendar",
   "alertHTML": "",
-  "footerHTML": "Made with ♥ by <a href=\"https://ko-fi.com/sant268">Sant268</a>",
+  "footerHTML": "Made with ♥ by <a href=\"https://ko-fi.com/sant268\">Sant268</a>",
   "daysToShow": 7,
   "useAlternateButtonStyle": false,
   "enableGoogleTranslate": true,
   "googleAnalyticsId": "",
   "googleCalendarApiKey": "",
-  "loadingImages": [
-    ],
-  "calendarSets": [
-    {
-    ]
+  "loadingImages": [],
+  "calendarSets": []
 };
